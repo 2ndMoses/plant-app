571ed698cdeca82dc4b3ade194e897f2cb766e59
2ndMoses <129158493+2ndMoses@users.noreply.github.com>
2023-04-20 22:48:56 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -14,45 +14,30 @@ from plant_components import *
 
 import logging
 import traceback
-#app = QApplication([])
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
+logging.debug("Application started")
 
-scene = QGraphicsScene()
-view = QGraphicsView(scene)
-view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-view.setRenderHint(QPainter.Antialiasing)
-
-# Add the plant container to the scene
 stem_position = QPointF(0, 0)
-initial_image_path = "C:/Users/Nick/Desktop/pictures/stem seedling"
-final_image_path = "C:/Users/Nick/Desktop/pictures/stem adult"
+initial_image_path = "path/to/initial_image.png"
+final_image_path = "path/to/final_image.png"
 min_height = 0
-max_height = 300
+max_height = 100
 
 
-# Add the plant container to the scene
 class CustomMainWindow(QMainWindow):
     def __init__(self, plant_growth_simulator, plant_container):
         super().__init__()
         self.plant_growth_simulator = plant_growth_simulator
         self.plant_container = plant_container
 
-        # Add the watering and fertilizing interactions to the plant container
-        self.watering_interaction = WateringInteraction(self.plant_container)
-        self.fertilizing_interaction = FertilizingInteraction(self.plant_container)
-
-        # Add hydration bar to the main window
-        seed = Seed.random_seed()
-        plant = Plant(seed)
-        hydration_bar = HydrationBar(plant)
-        plant.hydrationChanged.connect(hydration_bar.update_hydration)
+        # Add the watering interaction to the plant container
+        self.watering_interaction = WateringInteraction()
+        self.plant_container.addAction(self.watering_interaction.water_action)
 
         # Add the hydration bar to the main window layout
         layout = QVBoxLayout()
-        layout.addWidget(hydration_bar)
         layout.addWidget(self.plant_container)
 
         container = QWidget()
@@ -61,20 +46,16 @@ class CustomMainWindow(QMainWindow):
 
         logging.debug('customwindow')
 
-
     def contextMenuEvent(self, event):
         context_menu = QMenu(self)
 
-        # Add the generate seed action to the context menu
         generate_seed_action = QAction("Generate a Seed", self)
         generate_seed_action.triggered.connect(self.generate_random_seed)
         context_menu.addAction(generate_seed_action)
 
-        # Add the watering and fertilizing actions to the context menu
         context_menu.addAction(self.watering_interaction.water_action)
         context_menu.addAction(self.fertilizing_interaction.fertilize_action)
 
-        # Show the context menu
         context_menu.exec(event.globalPos())
 
         logging.debug('contextMenu')
@@ -85,14 +66,13 @@ class CustomMainWindow(QMainWindow):
         self.plant_container.generate_plant(random_seed)
 
 
-
-
 class PlantGrowthSimulator:
     def __init__(self):
         self.app = QApplication([])
         self.plant_container = PlantContainer(stem_position, initial_image_path, final_image_path, min_height, max_height)
 
         self.plant_growth_timing = PlantGrowthTiming(self.plant_container)
+        self.plant_growth_timing.start()
 
         self.main_window = CustomMainWindow(self, self.plant_container)
         self.main_window.setWindowTitle("Plant Growth Simulator")
@@ -103,7 +83,8 @@ class PlantGrowthSimulator:
 
         signal.signal(signal.SIGINT, self.handle_signal)
         signal.signal(signal.SIGTERM, self.handle_signal)
-        scene.addItem(self.plant_container)
+
+        logging.debug('PlantGrowthSimulator')
 
 
     def save_plant_state(self):
@@ -118,27 +99,54 @@ class PlantGrowthSimulator:
     def handle_signal(self, signum, frame):
         self.save_plant_state()
         sys.exit(0)
-    
+
     def run(self):
+        logging.debug("Application running")
+
+
+class PlantGrowthSimulator:
+    def __init__(self):
+        self.app = QApplication([])
+        self.plant_container = PlantContainer(stem_position, initial_image_path, final_image_path, min_height, max_height)
+
+        self.plant_growth_timing = PlantGrowthTiming(self.plant_container)
+        self.plant_growth_timing.start()
+
+        self.main_window = CustomMainWindow(self, self.plant_container)
+        self.main_window.setWindowTitle("Plant Growth Simulator")
+        self.main_window.setAttribute(Qt.WA_TranslucentBackground)
+        self.main_window.setCentralWidget(self.plant_container)
+
+        self.load_plant_state()
+
+        signal.signal(signal.SIGINT, self.handle_signal)
+        signal.signal(signal.SIGTERM, self.handle_signal)
+
+        logging.debug('PlantGrowthSimulator')
+
+    def save_plant_state(self):
+        PlantStateIO.save_plant_state(self.main_window, self.plant_container.current_plant)
+
+    def load_plant_state(self):
+        plant_state = PlantStateIO.load_plant_state(self.main_window)
+        if plant_state is not None:
+            self.plant_container.current_plant = plant_state
+            self.plant_container.update()
+
+    def handle_signal(self, signum, frame):
+        self.save_plant_state()
+        sys.exit(0)
+
+    def run(self):
+        logging.debug("Application running")
         try:
-            self.plant_growth_timing.start()
             self.main_window.show()
-            self.app.exec()
+            self.app.exec_()
         except Exception as e:
             logging.exception(f"Unhandled exception occurred: {e}")
             traceback.print_exc()
 
-container = PlantContainer(QPoint(400, 400), initial_image_path, final_image_path, min_height, max_height)
-scene.addItem(container)
-#stem = Stem(stem_position, initial_image_path, final_image_path, min_height, max_height)
-#stem.init_pixmaps()
-
-
 if __name__ == "__main__":
     simulator = PlantGrowthSimulator()
-  #  simulator.run()
-    try:
-        simulator.run()
-    except Exception as e:
-        logging.exception(f"Unhandled exception occurred: {e}")
-        traceback.print_exc()
+    simulator.run()
+
